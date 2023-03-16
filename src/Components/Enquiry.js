643511db4770714c851b59381c579780921266be
Manubolubhargav <bharga9911@gmail.com>
2023-03-16 12:28:38 +0530

    Enquiry component updated

--- src/Components/Enquiry.js
+++ src/Components/Enquiry.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
-import { Form, Button, Card, error } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 const validate = (values) => {
   const errors = {};
@@ -31,7 +31,7 @@ const Enquiry = () => {
     initialValues: {
       firstName: "",
       lastName: "",
-      phonenumber: "",
+      phonenumber: "+91 ",
       email: "",
     },
     validate,
@@ -41,94 +41,106 @@ const Enquiry = () => {
   });
   console.log("Form Values", formik.values);
   return (
-    <div>
-      <Card style={{ width: "30rem" }}>
-        <Card.Body>
-          <Card.Title>Get In Touch</Card.Title>
-          <Card.Text>If You Wont Any Enquiry Please Fill This Form</Card.Text>
-          <form onSubmit={formik.handleSubmit}>
-            <div className="form-control">
-              <label htmlFor="firstName"> * First Name :</label>
-              <input
-                id="firstName"
-                name="firstName"
-                type="text"
-                placeholder="Enter your First Name"
-                onChange={formik.handleChange}
-                value={formik.values.firstName}
-              />
-              {formik.errors.firstName ? (
-                <div className="error">{formik.errors.firstName}</div>
-              ) : null}
-            </div>
+    <div className="aboutcard mt-4">
+      <Row>
+        <Container fluid>
+          <Col md={{ span: 3, offset: 3 }}>
+            <Card style={{ width: "30rem" , backgroundColor:"black"}}>
+              <Card.Body>
+                <Card.Title style={{ color: "white" }}>Get In Touch</Card.Title>
+                <Card.Text style={{ color: "white" }}>
+                  If You Wont Any Enquiry Please Fill This Form
+                </Card.Text>
+                <form onSubmit={formik.handleSubmit}>
+                  <div >
+                    <label htmlFor="firstName" style={{ color: "white" }}> * First Name :</label>
+                    <input
+                      id="firstName"
+                      name="firstName"
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter your First Name"
+                      onChange={formik.handleChange}
+                      value={formik.values.firstName}
+                    />
+                    {formik.errors.firstName ? (
+                      <div className="error">{formik.errors.firstName}</div>
+                    ) : null}
+                  </div>
 
-            <div className="form-control">
-              <label htmlFor="lastName">* Last Name :</label>
-              <input
-                id="lastName"
-                name="lastName"
-                type="text"
-                placeholder="Enter your Last Name"
-                onChange={formik.handleChange}
-                value={formik.values.lastName}
-              />
-              {formik.errors.lastName ? (
-                <div className="error">{formik.errors.lastName}</div>
-              ) : null}
-            </div>
-            <br />
-            <div className="form-control">
-              <lable htmlFor="phonenumber"> * Phone Number :</lable>
-              <br />
-              <input
-                type="tel"
-                id="phonenumber"
-                name="phonenumber"
-                placeholder="Enter your phone number"
-                onChange={formik.handleChange}
-                value={formik.values.phonenumber}
-              />
-              {formik.errors.phonenumber ? (
-                <div className="error">{formik.errors.phonenumber}</div>
-              ) : null}
-            </div>
-            <br />
-            <div className="form-control">
-              <label htmlFor="email"> * Email Address :</label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="Enter your Email Address"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-              />
-              {formik.errors.email ? (
-                <div className="error">{formik.errors.email}</div>
-              ) : null}
-            </div>
-            <br />
+                  <div >
+                    <label htmlFor="lastName" style={{ color: "white" }}>* Last Name :</label>
+                    <input
+                      id="lastName"
+                      name="lastName"
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter your Last Name"
+                      onChange={formik.handleChange}
+                      value={formik.values.lastName}
+                    />
+                    {formik.errors.lastName ? (
+                      <div className="error" >{formik.errors.lastName}</div>
+                    ) : null}
+                  </div>
+                  <br />
+                  <div>
+                    <lable htmlFor="phonenumber" style={{ color: "white" }}> * Phone Number :</lable>
+                    <br />
+                    <input
+                      type="tel"
+                      id="phonenumber"
+                      name="phonenumber"
+                      className="form-control"
+                      placeholder="Enter your phone number"
+                      onChange={formik.handleChange}
+                      value={formik.values.phonenumber}
+                    />
+                    {formik.errors.phonenumber ? (
+                      <div className="error">{formik.errors.phonenumber}</div>
+                    ) : null}
+                  </div>
+                  <br />
+                  <div>
+                    <label htmlFor="email" style={{ color: "white" }}> * Email Address :</label>
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      className="form-control"
+                      placeholder="Enter your Email Address"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                    />
+                    {formik.errors.email ? (
+                      <div className="error">{formik.errors.email}</div>
+                    ) : null}
+                  </div>
+                  <br />
 
-            <div className="form-control">
-              <lable htmlFor="Message">Message : </lable>
-              <br />
-              <input
-                as="message"
-                rows={3}
-                placeholder="Enter your message"
-                onChange={formik.handleChange}
-                value={formik.values.message}
-              />
-            </div>
-            <br />
-            <br />
-            <button type="submit">Submit</button>
-          </form>
-          <br />
-
-          <div> * Fields Marked with * will be Mandatory </div>
-        </Card.Body>
-      </Card>
+                  <div>
+                    <lable htmlFor="Message" style={{ color: "white" }}>Message : </lable>
+                    <br />
+                    <input
+                      id="Message"
+                      name="Message"
+                      type="Message"
+                      className="form-control"
+                      placeholder="Enter your Message"
+                      onChange={formik.handleChange}
+                      value={formik.values.Message}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="button">Submit</button>
+                </form>
+                <br />
+                <div style={{ color: "white" }}> * Fields Marked with * will be Mandatory </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Container>
+      </Row>
     </div>
   );
 };
